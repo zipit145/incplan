30fc56fef83a5af256b97857366e78a75f4998f6
zipit145 <gregbforney@gmail.com>
2019-03-20 15:44:56 -0600

    Basic layout and set up. Installed some packages.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import Home from './component/Home'
 import ClientHome from './component/ClientHome'
 import Header from './component/Header'
 import Footer from './component/Footer'
+//import Button from 'react-bootstrap/Button';
+import { Button } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 
 
 
@@ -12,10 +18,16 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
-        <Home />
-        <ClientHome />
-        <Footer />
+
+            <Header />
+            <Home />
+            <ClientHome />
+            <Button />
+            <Footer />
+
+
+
+
       </div>
     );
   }
--- src/component/Footer.js
+++ src/component/Footer.js
@@ -2,8 +2,7 @@ import React from 'react';
 
 const Footer = (props) => {
     return (
-      <div>
-          Footer
+      <div className="footer">
       </div>
     )
   }
